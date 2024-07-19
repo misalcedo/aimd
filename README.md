857fd92aaaa93cfd820f308dfb8b1715e849e7c7
Miguel D. Salcedo <misalcedo@github.com>
2024-07-19 10:19:43 -0400

    Update readme and simulation.

--- README.md
+++ README.md
@@ -11,11 +11,24 @@ See https://en.wikipedia.org/wiki/Additive_increase/multiplicative_decrease
 The example directory contains a simulation of multiple concurrent workers along with a CSV of a run using the default parameters.
 Each worker has its own limiter and attempts to run a number of concurrent operations.
 
+### Run 1
 The following chart shows the maximum concurrency limit of each worker on a 1 second interval.
 The simulation was run with 10 workers each with a concurrency of 1,000 each for 10,000 iterations per concurrent operation.
 The underlying resource has a concurrency of 1,000.
 
 ![Chart of worker concurrency limits](example/chart.png)
+
+### Run 2
+The following chart shows the maximum concurrency limit of each worker (i.e. client).
+The simulation was run with 10 workers each with a concurrency of 100 each for 100 iterations per concurrent operation.
+The underlying resource has a concurrency of 100.
+
+The following chart shows the client's utilization of the server.
+The values don't perfectly match the server's utilization due to the grouping used for client utilization values.
+![Chart of worker concurrency limits](example/client.png)
+
+The following chart shows the server's utilization.
+![Chart of server utilization](example/server.png)
 
 ## Acknowledgement
 This code is based a fork of [golang.org/x/sync/semaphore#Weighted](https://pkg.go.dev/golang.org/x/sync@v0.7.0/semaphore#Weighted).
--- example/main.go
+++ example/main.go
@@ -14,10 +14,10 @@ func main() {
 	var wg sync.WaitGroup
 	var concurrency, iterations, rate, workers int
 
-	flag.IntVar(&iterations, "iterations", 10_000, "number of iterations")
+	flag.IntVar(&iterations, "iterations", 100, "number of iterations")
 	flag.IntVar(&rate, "rate", 100, "rate of the underlying resource")
 	flag.IntVar(&workers, "workers", 10, "number of workers")
-	flag.IntVar(&concurrency, "concurrency", 1_000, "concurrency of a worker")
+	flag.IntVar(&concurrency, "concurrency", 100, "concurrency of a worker")
 	flag.Parse()
 
 	if rate >= workers*concurrency {
@@ -26,13 +26,11 @@ func main() {
 
 	root := aimd.NewLimiter(rate, 0, 1)
 
-	fmt.Println("X,ID,Limit,Acquired")
-
-	var limiters []*aimd.Limiter
+	start := time.Now()
+	metrics := make(chan [4]int, workers*concurrency*iterations)
 
 	for i := 0; i < workers; i++ {
 		limiter := aimd.NewLimiter(1, 1, 2)
-		limiters = append(limiters, limiter)
 
 		for j := 0; j < concurrency; j++ {
 			wg.Add(1)
@@ -41,6 +39,8 @@ func main() {
 
 				for k := 0; k < iterations; k++ {
 					_ = limiter.Acquire(context.Background(), 1)
+
+					metrics <- [4]int{int(time.Since(start).Nanoseconds()), id, 1, root.Acquired()}
 
 					if root.TryAcquire(1) {
 						limiter.ReleaseSuccess(1)
@@ -53,20 +53,12 @@ func main() {
 		}
 	}
 
-	go func() {
-		ticker := time.NewTicker(time.Second)
-		defer ticker.Stop()
-
-		for range ticker.C {
-			now := time.Now().Format(time.DateTime)
-
-			fmt.Printf("%s,%d,%d,%d\n", now, 0, root.Size(), root.Acquired())
-
-			for i, limiter := range limiters {
-				fmt.Printf("%s,%d,%d,%d\n", now, i+1, limiter.Size(), limiter.Acquired())
-			}
-		}
-	}()
-
 	wg.Wait()
+
+	close(metrics)
+
+	fmt.Println("X,ID,Client,Server")
+	for entry := range metrics {
+		fmt.Printf("%d,%d,%d,%d\n", entry[0]%10_000, entry[1], entry[2], entry[3])
+	}
 }
